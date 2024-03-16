3bca6e6b3a0df129e2b9c4493f942fd091db77be
Zamperi <samuli.kokko@hotmail.com>
2024-03-16 10:07:17 +0200

    classExample on aloitettu

--- teht0/main.cpp
+++ teht0/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <cstdlib> // Hox uusi kirjasto!
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+void randomNumberSequence(int seedNumber) {
+    // Asetetaan siemenluku
+    srand(seedNumber);
+
+    for (int i = 0; i<5;i++){
+        int randomNumber = rand() % 20;
+        cout<<randomNumber<<endl;
+    }
+}
+
+int main() {
+    randomNumberSequence(54);
     return 0;
 }
